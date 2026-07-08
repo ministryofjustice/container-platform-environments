b3c683357201f98d8bae42539ef1bfd4b5ca8a71
David Elliott <david.elliott@digital.justice.gov.uk>
2026-07-08 16:27:14 +0100

    Initial identity commit

--- identity/terraform/outputs.tf
+++ identity/terraform/outputs.tf
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
 # Debug output removed to avoid persisting aggregated namespace manifests in Terraform state and CI logs.
-=======
-output "namespaces_debug" {
-  description = "Temporary debug output for aggregated namespace manifests"
-  value       = local.namespaces
-}
->>>>>>> d49c90b (Initial set up and identity)
