7e0c7bd7f46929cadce8e4048d2b8b81141ceed7
David Elliott <david.elliott@digital.justice.gov.uk>
2026-07-17 16:43:34 +0100

    Add LAA and HMPPS BUs to identity
    Also refactoring to make code a bit clearer.

--- identity/terraform/eks_access_entries.tf
+++ identity/terraform/eks_access_entries.tf
@@ -39,3 +39,87 @@ module "namespace_team_eks_access_entries_octo_live" {
 
   depends_on = [module.namespace_team_readonly_eks]
 }
+
+module "namespace_team_eks_access_entries_hmpps_nonlive" {
+  for_each = local.namespace_access_teams
+  source   = "./modules/eks_access_entries"
+
+  providers = {
+    aws.eks_access = aws.hmpps-nonlive-eks-access
+  }
+
+  permission_set_name = local.namespace_team_permission_set_names[each.key]
+  assignments = {
+    for key, assignment in local.namespace_team_access_assignments_hmpps_nonlive :
+    key => {
+      cluster          = assignment.cluster
+      kubernetes_group = assignment.kubernetes_group
+    }
+    if assignment.kubernetes_group == each.key
+  }
+
+  depends_on = [module.namespace_team_readonly_eks]
+}
+
+module "namespace_team_eks_access_entries_hmpps_live" {
+  for_each = local.namespace_access_teams
+  source   = "./modules/eks_access_entries"
+
+  providers = {
+    aws.eks_access = aws.hmpps-live-eks-access
+  }
+
+  permission_set_name = local.namespace_team_permission_set_names[each.key]
+  assignments = {
+    for key, assignment in local.namespace_team_access_assignments_hmpps_live :
+    key => {
+      cluster          = assignment.cluster
+      kubernetes_group = assignment.kubernetes_group
+    }
+    if assignment.kubernetes_group == each.key
+  }
+
+  depends_on = [module.namespace_team_readonly_eks]
+}
+
+module "namespace_team_eks_access_entries_laa_nonlive" {
+  for_each = local.namespace_access_teams
+  source   = "./modules/eks_access_entries"
+
+  providers = {
+    aws.eks_access = aws.laa-nonlive-eks-access
+  }
+
+  permission_set_name = local.namespace_team_permission_set_names[each.key]
+  assignments = {
+    for key, assignment in local.namespace_team_access_assignments_laa_nonlive :
+    key => {
+      cluster          = assignment.cluster
+      kubernetes_group = assignment.kubernetes_group
+    }
+    if assignment.kubernetes_group == each.key
+  }
+
+  depends_on = [module.namespace_team_readonly_eks]
+}
+
+module "namespace_team_eks_access_entries_laa_live" {
+  for_each = local.namespace_access_teams
+  source   = "./modules/eks_access_entries"
+
+  providers = {
+    aws.eks_access = aws.laa-live-eks-access
+  }
+
+  permission_set_name = local.namespace_team_permission_set_names[each.key]
+  assignments = {
+    for key, assignment in local.namespace_team_access_assignments_laa_live :
+    key => {
+      cluster          = assignment.cluster
+      kubernetes_group = assignment.kubernetes_group
+    }
+    if assignment.kubernetes_group == each.key
+  }
+
+  depends_on = [module.namespace_team_readonly_eks]
+}
